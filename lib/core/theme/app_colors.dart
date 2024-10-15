04826e973ca1714dd85ca092febb73282bb7b542
unknown <hesenovadil757@gmail.com>
2024-10-15 04:01:48 +0400

    - Setup , Added static constants

---
+++ lib/core/theme/app_colors.dart
@@ -0,0 +1,13 @@
+import 'package:flutter/widgets.dart';
+
+class AppColors {
+  /// Brend
+  static const brendDark = Color(0xFF096C68);
+  static const brendDefault = Color(0xFF24A19C);
+  static const brendBackgraund = Color(0xFFF2F9F9);
+
+  /// Neutral
+  static const neutralDark = Color(0xFF1B1C1F);
+  static const neutralSecondary = Color(0xFF767E8C);
+  static const neutralGhost = Color(0xFFA9B0C5);
+}
